091e7d31ba5a236ce6b62bd03f427cd9e137bfe9
Jairo Mendes <jairomfj@gmail.com>
2017-07-18 00:05:22 -0300

    problem 2 adjustements

--- src/main/scala/br/com/study/Problem2.scala
+++ src/main/scala/br/com/study/Problem2.scala
@@ -2,10 +2,10 @@ package br.com.study
 
 object Problem2 {
 
-  def execute(max: Long, value1: Long = 0, value2: Long = 1, sum: Long = 0): Long = {
+  def execute(max: Long, value1: Long = 0, value2: Long = 1, evenSum: Long = 0): Long = {
     val result = value1 + value2
-    if(result >= max) sum
-    else if(result % 2 == 0) result + execute(max, value2, value1 + value2, sum)
-    else execute(max, value2, value1 + value2, sum)
+    if(result >= max) evenSum
+    else if(result % 2 == 0) result + execute(max, value2, result, evenSum)
+    else execute(max, value2, result, evenSum)
   }
 }
